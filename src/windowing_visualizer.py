4619d98f008ddd05a910475b903052ffa9536513
kcisek <kamil.cisek03@gmail.com>
2025-12-08 22:56:48 +0100

    Pix2Pix model for 128x128 images

--- src/windowing_visualizer.py
+++ src/windowing_visualizer.py
@@ -6,7 +6,7 @@ from dival import get_standard_dataset
 import torch
 from torch.utils.data import DataLoader
 
-from models.Pix2PixV1 import UnetGenerator
+from models.Pix2Pix_256_V1 import UnetGenerator
 
 
 def slider_window(image1, image2, a0=-1024, b0=3071):
